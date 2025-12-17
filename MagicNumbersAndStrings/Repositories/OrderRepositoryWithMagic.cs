019b202fb8ec5734b3552b06ee48aa094e635d5f
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2025-12-17 17:43:36 +0400

    asdadadasd

--- MagicNumbersAndStrings/Repositories/OrderRepositoryWithMagic.cs
+++ MagicNumbersAndStrings/Repositories/OrderRepositoryWithMagic.cs
@@ -11,25 +11,14 @@ namespace MagicNumbersAndStrings.Repositories
 {
     internal class OrderRepositoryWithMagic : IOrderRepository
     {
-        const decimal orderValueThreshold = 1000;
-        const decimal orderDiscount = 0.1m;
         public string ProccessOrder(Order ord)
         {
             if (ord.Amount > 1000)
                 ord.Discount = ord.Amount * 0.1m;//0.1m- Magic numbers
 
-            ord.OrderStatus = Order.Status.Processing;//"Processing"- Magic strings
-            var res = $"Enum(string): {ord.OrderStatus}{(char)13}{(char)10}Enum display name: {GetEnumDisplayName(ord.OrderStatus)}";
+            ord.StatusTxt = "Processing";//"Processing"- Magic strings
 
-            return res;
-        }
-
-        public string GetEnumDisplayName(Enum value)
-        {
-            var field = value.GetType().GetField(value.ToString());
-            var attr = field.GetCustomAttribute<DisplayAttribute>();
-
-            return attr?.Name ?? value.ToString();
+            return ord.StatusTxt;
         }
     }
 }
