e4ce9ce352dd8a3edeaed1dd6343e12a2d2e1553
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2025-12-23 11:00:34 +0400

    C# Keywords

---
+++ CsharpKeywords/Keywords/CombiningKeyword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpKeywords.Keywords
+{
+    public class CombiningKeyword
+    {
+        public string CheckMultipleType(object obj)
+        {
+            var res = obj switch
+            {
+                int value when value is > 0 => "Positive integer",
+                int value when value is < 0 => "Negative integer",
+                string str when str is { Length: 0 } or { Length: 1 } => "Empty or single character string",
+                string str when str is { Length: > 1 } => "String with more than one character",
+                not null => "Some other non-null type",
+                _ => "Null value"
+            };
+
+            return res;
+        }
+    }
+}
---
+++ CsharpKeywords/Keywords/IsMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpKeywords.Keywords
+{
+    public class IsMethods
+    {
+        public string CheckValueTypeFirst(object obj)
+        {
+            var res = string.Empty;
+            if (obj is string)
+                res = "The object is a string";
+
+            return res;
+        }
+
+        public string CheckValueTypeSecond(object obj)
+        {
+            var res = string.Empty;
+            if (obj is string s)
+                res = $"string type:{s}";
+
+            return res;
+        }
+    }
+}
