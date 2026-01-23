96b98b24718485ede7c9a3db35739efdf9f93b46
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2026-01-23 12:49:29 +0400

    hhhhhhhhhh

--- PropertyPattern/Program.cs
+++ PropertyPattern/Program.cs
@@ -7,21 +7,21 @@ namespace PropertyPattern
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            var clsTraditional = new User
+            var cls = new User
             {
                 Role = "Admin",
                 Age = 25
             };
 
             #region Traditional Approach            
-            if (clsTraditional is not null && clsTraditional.Role.Equals("Admin") && clsTraditional.Age > 21)
+            if (cls is not null && cls.Role.Equals("Admin") && cls.Age > 21)
                 Console.WriteLine("Grant admin access(Traditional Approach)");
             #endregion
 
-            clsTraditional = null;
+            cls = null;
 
             #region Property Pattern
-            if (clsTraditional is { Role: "Admin", Age: > 21 })
+            if (cls is { Role: "Admin", Age: > 21 })
                 Console.WriteLine("Grant admin access(Property Pattern)");
             #endregion
         }
