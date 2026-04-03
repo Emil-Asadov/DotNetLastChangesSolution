0a0db631dfbe98428531b8c9aba5e01e79c4f059
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2026-04-03 09:59:11 +0400

    dsfsfdfds

---
+++ RSAKeyOpenSSL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace RSAKeyOpenSSL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var rsa = RSA.Create(2048);
+            var publicKey = rsa.ExportSubjectPublicKeyInfo();
+            var pem = PemEncoding.Write("PUBLIC KEY", publicKey);
+
+            Console.WriteLine(pem);
+        }
+    }
+}
