f4975854d2915b677ba70576d6b32f4289a02270
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2026-04-14 15:25:01 +0400

    dddddddd

---
+++ PostmanEnvironmentTestApi/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace PostmanEnvironmentTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        [Route("Calculate")]
+        public IActionResult Calculate(int a, int b)
+        {
+            var res = a + b;
+            return Ok(res);
+        }
+        [HttpPost]
+        [Route("Concate")]
+        public IActionResult Concate(string name, string birthDate)
+        {
+            var res = new StringBuilder().Append(name).Append((char)32).Append(birthDate);
+            return Ok(res.ToString());
+        }
+        [HttpGet]
+        [Route("CheckCurrentDate/{currentDateStr}")]
+        public IActionResult Calculate(string currentDateStr)
+        {
+            var res = DateTime.TryParse(currentDateStr, out var currentDate);
+            if (!res)
+                return NotFound();
+
+            res = currentDate.Equals(DateTime.Now.Date);
+
+            return Ok(res);
+        }
+    }
+}
