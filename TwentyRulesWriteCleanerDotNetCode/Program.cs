192246b58cd3554c6871c2dd5430f53c5e588d8b
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2026-05-04 09:03:43 +0400

    asdasdasdsa

---
+++ TwentyRulesWriteCleanerDotNetCode/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TwentyRulesWriteCleanerDotNetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var inp = "ORD-12345";
+            var id = inp.Substring(4);
+            var parsed = int.Parse(id);
+
+            ReadOnlySpan<char> input = inp;
+            parsed = int.Parse(input[4..]);
+        }
+    }
+}
