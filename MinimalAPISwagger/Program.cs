2b6ec43c1ef79ff98758dcdaca96c41997e3eb9b
E.Q.Asadov <E.Q.Asadov@RB00-ITD011>
2026-06-16 16:37:38 +0400

    Swagger added

---
+++ MinimalAPISwagger/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region MinimalAPI Swagger
+builder.Services.AddEndpointsApiExplorer();//Endpoint-leri gostermek ucun
+builder.Services.AddSwaggerGen();
+#endregion
+
+var app = builder.Build();
+
+#region MinimalAPI Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+#endregion
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("simple-string", () => "Hello World");
+app.MapGet("json-raw-obj", () => new { Message = "Hello World" });
+app.MapGet("ok-obj", () => Results.Ok(new { Message = "Hello World" }));
+app.MapGet("json-obj", () => Results.Json(new { Message = "Hello World" }));
+app.MapGet("text-string", () => Results.Text("Hello World"));
+
+app.Run();
